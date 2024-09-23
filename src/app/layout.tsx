f4859e07e32a4991259a202dfedfd95e0e57b7c2
wd021 <dannywitters@gmail.com>
2024-09-23 23:14:57 +0100

    update metadata

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,8 +2,8 @@ import type { Metadata } from "next";
 import "./globals.css";
 
 export const metadata: Metadata = {
-  title: "This for That",
-  description: "An Open Marketplace Where We Swap NFTs for NFTs",
+  title: "TFT",
+  description: "A Space Where We Swap NFTs for NFTs",
 };
 
 export default function RootLayout({
